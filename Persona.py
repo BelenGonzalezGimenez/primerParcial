6b6d46692ed60e7975b5c6f7f521410e2789cd32
Ana Belen Gonzalez <belen280911@fpuna.edu.py>
2023-08-22 10:20:22 -0400

    commit:creacion de clases

---
+++ Persona.py
@@ -0,0 +1,11 @@
+class Persona():
+    def __init__(self, cedula, nombre):
+        self.cedula = cedula
+        self.nombre = nombre
+        self.fecha_creacion
+        self.fecha_modificacion
+
+    def __str__(self) -> str:
+        print(f"Cedula : {self.cedula} \n Nombre : {self.nombre}")
+
+
---
+++ Producto.py
@@ -0,0 +1,5 @@
+class Producto ():
+    def __init__(self, fecha_creacion, descripcion) -> None:
+        self.fecha_creacion = fecha_creacion
+        self.descripcion = descripcion
+        
